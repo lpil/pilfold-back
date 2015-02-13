20a37334c4686293d0fbf8d289f9aa3c63e488be
Louis Pilfold <louis@lpil.uk>
2015-02-13 23:46:08 +0000

    Fiddle with params. Enable logging

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
+import Network.Wai.Middleware.RequestLogger
 import qualified Network.HTTP.Types as HTTP
-import qualified Data.Map           as Map
+
+import qualified Data.Char as Char
+import qualified Data.Map  as Map
 
 main = scotty 3000 $ do
+  middleware logStdoutDev
   get "/" $
     json ("Hello, world!" :: String)
+
+  get "/:name" $ do
+    name <- param "name"
+    json ("Hello, " ++ name ++ "!" :: String)
 
   notFound $ do
     status HTTP.status404
