de420cee24f490a8bb0669e9a6f3c3c7b41ebc21
Louis Pilfold <louis@lpil.uk>
2015-02-14 00:01:02 +0000

    Refactor into smaller chunks

--- src/main.hs
+++ src/main.hs
@@ -6,18 +6,31 @@ import qualified Network.HTTP.Types as HTTP
 import qualified Data.Char as Char
 import qualified Data.Map  as Map
 
-main = scotty 3000 $ do
+
+pilfoldBackend :: ScottyM ()
+pilfoldBackend = do
   middleware logStdoutDev
-  get "/" $
-    json ("Hello, world!" :: String)
 
-  get "/:name" $ do
-    name <- param "name"
-    json ("Hello, " ++ name ++ "!" :: String)
-
-  notFound $ do
-    status HTTP.status404
-    json $ strMap [("error", "Resource not found!")]
+  get "/"      helloWorld
+  get "/:name" helloYou
+  notFound     showError
 
 strMap :: [(String, String)] -> Map.Map String String
 strMap = Map.fromList
+
+
+helloWorld :: ActionM ()
+helloWorld = json ("Hello, world!" :: String)
+
+helloYou :: ActionM ()
+helloYou = do
+    name <- param "name"
+    json ("Hello, " ++ name ++ "!" :: String)
+
+showError :: ActionM ()
+showError = do
+    status HTTP.status404
+    json $ strMap [("error", "Resource not found!")]
+
+main :: IO ()
+main = scotty 3000 pilfoldBackend
